8f2e93de360b6345913665c68c863cfef62b9028
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-16 10:21:47 +0100

    Criado a minha estrutura de navegação tabs

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,24 @@
+import { theme } from '@/theme'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default function Messages() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Profile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.gray[800],
+  },
+  text: {
+    fontSize: 22,
+    color: theme.colors.white,
+    fontFamily: theme.fontFamily.regular,
+  },
+})

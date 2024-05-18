da02647dc8c36f661a797d941c302ef5b3de5ba1
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-18 19:31:54 +0100

    Adicionado o meu skeleton

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View, useWindowDimensions } from 'react-native'
 import { useEffect } from 'react'
+import { StyleSheet, View, useWindowDimensions } from 'react-native'
+import { Skeleton } from 'moti/skeleton'
 
 import { theme } from '@/theme'
 
@@ -15,6 +16,12 @@ export default function Splash() {
   const logoPositionY = useSharedValue(0)
 
   const dimensions = useWindowDimensions()
+
+  const skeletonColors = [
+    theme.colors.gray[600],
+    theme.colors.gray[700],
+    theme.colors.gray[600],
+  ]
 
   const logoAnimatedStyles = useAnimatedStyle(() => ({
     transform: [
@@ -38,6 +45,35 @@ export default function Splash() {
     )
   }
 
+  function filters() {
+    return Array.from({ length: 10 }).map((_, index) => (
+      <Skeleton
+        key={index}
+        width={60}
+        height={26}
+        radius={6}
+        colors={skeletonColors}
+      />
+    ))
+  }
+
+  // Criando o meu Skeleton
+  function boxes(column: 'right' | 'left') {
+    const rest = column === 'left' ? 0 : 1
+
+    return Array.from({ length: 20 })
+      .filter((_, index) => index % 2 === rest)
+      .map((_, index) => {
+        const height = index % 2 === (column === 'left' ? 0 : 1) ? 200 : 300
+
+        return (
+          <Animated.View key={index} style={[styles.box, { height }]}>
+            <Skeleton colors={skeletonColors} width="100%" height={height} />
+          </Animated.View>
+        )
+      })
+  }
+
   useEffect(() => {
     logoAnimation()
   }, [])
@@ -48,12 +84,16 @@ export default function Splash() {
         source={require('@/assets/logo.png')}
         style={[styles.logo, logoAnimatedStyles]}
       />
+
+      <View style={styles.header}>{filters()}</View>
+
+      <View style={styles.boxes}>
+        <View style={styles.column}>{boxes('left')}</View>
+        <View style={styles.column}>{boxes('right')}</View>
+      </View>
     </View>
   )
 }
-
-// abelmakengo1996@gmail.com
-// AbelMakengo@#07
 
 const styles = StyleSheet.create({
   container: {
@@ -61,9 +101,31 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.black,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 12,
   },
   logo: {
     width: 64,
     height: 64,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 16,
+    paddingBottom: 12,
+  },
+  boxes: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+  },
+  box: {
+    width: '100%',
+    borderRadius: 16,
+    backgroundColor: theme.colors.gray[600],
+  },
+  column: {
+    flex: 1,
+    gap: 12,
   },
 })
